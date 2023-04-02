465e9cd285e7a30dcb95ab7b51a20d44d8673a41
K0dred <101974086+K0dred@users.noreply.github.com>
2023-04-02 19:31:44 +0300

    Update reklama frfa.js

--- reklama frfa.js
+++ reklama frfa.js
@@ -152,14 +152,14 @@
         };
        RegisterScript("Reklama_FRFA");
 
-function SendPhrases(allyChat = false, index = null) {
-    Chat.Say(allyChat ? 'DOTAChannelType_GameAllies' : 'DOTAChannelType_GameAll', allPhrases[phraseIndex]);
-    phraseIndex = (phraseIndex + 1) % allPhrases.length;
+ function SendPhrases(allyChat = false, index = null) {
+    Chat.Say(allyChat ? 'DOTAChannelType_GameAllies' : 'DOTAChannelType_GameAll', allPhrases[(phraseIndex + 1) % allPhrases.length]);
 }
 
 let allPhrases = [
     'Сосешь хуй? не беда',
-    'С новыми скриптами от Матвея (no_FRFA), ты сможешь наконец-то забыть вкус хуя и выигрывать'
+    'С новыми скриптами от Матвея (no_FRFA)''
+  'ты сможешь наконец-то забыть вкус хуя и выигрывать''
 ];
 
     })(mamaSorry || (mamaSorry = {}));
